9bd32e33a0bb2a194457410ef2bfeecb6eadd5bd
leonardo campanale <leo.campanale@gmail.com>
2016-11-11 10:53:25 +0100

    test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: leonardo
+ *
+ * Created on 11 novembre 2016, 10.52
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
